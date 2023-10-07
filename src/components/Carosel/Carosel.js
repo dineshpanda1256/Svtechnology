3b0a3a9f711611adcbef68dcf3f7e035f76044e7
Debashish2022 <114467414+Debashish2022@users.noreply.github.com>
2023-10-07 19:40:15 +0530

    Done

--- src/components/Carosel/Carosel.js
+++ src/components/Carosel/Carosel.js
@@ -1,25 +1,38 @@
-import React from 'react'
-import { Carousel } from 'react-bootstrap'
+import React, { useState, useEffect } from "react";
+import { Carousel } from "react-bootstrap";
 import "./Carosel.css";
+import { DriverController } from "../../redux/controllers/DriverController";
 
 export default function Carosel() {
-    
+  const [images, setImages] = useState([]);
 
-    const data =[
-        {
-            image:require("../../assets/image/home/carosel.png")
-        },
-        {
-            image:require("../../assets/image/header/Logo.png")
-        }
-    ]
+  useEffect(() => {
+    getbanner();
+  }, []);
+
+  const getbanner = () => {
+    DriverController.getAllbanners()
+      .then((res) => {
+        setImages(res?.data);
+        // console.log("Images is......",setImages);
+      })
+      .catch((error) => {
+        console.error("Error fetching images:", error);
+      });
+  };
+
   return (
     <Carousel fade>
-    {data?.map((item) => (
-      <Carousel.Item interval={1000}>
-        <img className="d-block w-100" src={item.image} alt="First slide" id="carosel1" />
-      </Carousel.Item>
-    ))}
-  </Carousel>
-  )
+      {images?.map((item) => (
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            src={item.image}
+            alt="First slide"
+            id="carosel1"
+          />
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
 }
